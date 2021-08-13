c3deb10c563dcd88bc08df7ae28d8557d7e3c073
김연구 <yeongoo.kim@qualson.com>
2021-08-13 21:37:07 +0900

    feat: react hook form

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,17 +1,20 @@
 import * as S from './Input.style'
-
+import { UseFormRegisterReturn } from 'react-hook-form'
+import { FormData } from '@pages/write/Write'
 interface Props {
   label?: string
   type?: string
   placeholder?: string
+  register: UseFormRegisterReturn
+  id?: keyof FormData
 }
 
-export default function Input({ label, type = 'text', placeholder }: Props) {
+export default function Input({ id, label, type = 'text', placeholder, register }: Props) {
   return (
     <S.Container>
       <S.Label>
         {label}
-        <S.Input type={type} placeholder={placeholder} />
+        <S.Input id={id} type={type} placeholder={placeholder} {...register} />
       </S.Label>
     </S.Container>
   )
--- client/src/pages/write/Write.tsx
+++ client/src/pages/write/Write.tsx
@@ -3,17 +3,30 @@ import Input from '@components/Input'
 import MarkdownEditor from './components/MarkdownEditor'
 import * as CS from './common.style'
 import Button from '@components/Button'
+import { useForm } from 'react-hook-form'
+
+export interface FormData {
+  title: string
+  content: React.ReactNode
+}
 
 export default function Write() {
+  const { register, handleSubmit } = useForm<FormData>({ mode: 'onBlur' })
+
+  const onSubmit = (form: FormData) => console.log(form)
   return (
     <>
       <Header />
-      <Input placeholder="제목을 입력해주세요." />
-      <MarkdownEditor onChange={(v) => console.log(v)} />
-      <CS.ButtonGroup>
-        <Button color="#000">저장</Button>
-        <Button color="#000">취소</Button>
-      </CS.ButtonGroup>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input id="title" placeholder="제목을 입력해주세요." register={register('title')} />
+        <MarkdownEditor onChange={(v) => console.log(v)} />
+        <CS.ButtonGroup>
+          <Button type="submit" color="#000">
+            저장
+          </Button>
+          <Button color="#000">취소</Button>
+        </CS.ButtonGroup>
+      </form>
     </>
   )
 }
